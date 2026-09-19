e318d2e3004071f57c76cc0f1f928eb74c1d2902
Pooja Paramashiva <pooja26.5.2006@gmail.com>
2026-09-20 01:19:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+      host: '0.0.0.0',
+      port: parseInt(process.env.PORT || '10000'),
+      allowedHosts: ['coorg-hill-view-resort-02n4.onrender.com', 'localhost'],
+    }
 })
